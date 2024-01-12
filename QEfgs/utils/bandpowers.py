4a46a6c9cd6316d37458ea1e711cef6282fa5f30
Irene Abril Cabezas <79768254+iabrilcabezas@users.noreply.github.com>
2024-01-12 10:40:27 -0800

    lensing fac

--- QEfgs/utils/bandpowers.py
+++ QEfgs/utils/bandpowers.py
@@ -14,8 +14,9 @@ def get_ell_arrays(lmax):
 
     L = np.linspace(0, lmax, lmax+1)
     Lfac = (L * (L+1))**2 / (2*np.pi)
+    Llens_fac = L**(1/2) * (L * (L+1))**2 / (2 * np.pi)
 
-    return L, Lfac
+    return L, Lfac, Llens_fac
 
 def get_ell_bins():
 
